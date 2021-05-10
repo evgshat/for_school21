93e2fc949e6b6d178a447940c2088385c5186716
Laucher Charlette <lcharlet@at-l1.msk.21-school.ru>
2021-05-10 14:49:47 +0300

    15

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -3,47 +3,20 @@
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	int		i;
-	int		n;
-	int		cnt;
 	char	*new_s;
 
 	i = 0;
-	n = 0;
-	cnt = 0;
 	while (s[i] != '\0')
-	{
-		cnt++;
 		i++;
-	}
-	new_s = (char *)malloc(cnt * sizeof(char));
+	new_s = (char *)malloc(i * sizeof(char) + 1);
 	if (new_s == 0)
 		return (0);
 	i = 0;
 	while (s[i] != '\0')
 	{
-		new_s[i] = f(cnt, s[i]);
+		new_s[i] = f(i, s[i]);
 		i++;
 	}
+	new_s[i] = '\0';
 	return (new_s);
 }
-
-char	f(unsigned int cnt, char s)
-{
-	char	new_s;
-	int		i;
-
-	i = 0;
-	//
-	cnt = 0;
-	new_s = (char) malloc(1 * sizeof(char));
-	if (new_s == 0)
-		write(1, "NULL", 5);
-	new_s = s + '0';
-	return (new_s);
-}
-/*
-int	main(void)
-{
-	printf ("%s\n", ft_strmapi("abc", f));
-}
-*/
